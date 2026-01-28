b46f28678cad2b9299a05f14c061aaeaec893ee1
dinsaradasin <dinsaradasin@gmail.com>
2026-01-28 17:14:33 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -82,7 +82,7 @@ CREATE TABLE appointment (
 
 CREATE TABLE bill (
     bill_id INT AUTO_INCREMENT PRIMARY KEY,
-    bill_date DATETIME,
+    bill_date DATE,
     total_amount DECIMAL(10, 2),
     bill_status VARCHAR(20) DEFAULT 'Unpaid',
     app_id INT,
